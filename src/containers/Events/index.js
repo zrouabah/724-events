b3dd3d6237262082094c516877b9dcb8cda6119d
Rouabah <rouabah.zineb@hotmail.fr>
2024-02-22 14:11:15 +0100

    added filter to data.events for const EventList

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -4,7 +4,6 @@ import Select from "../../components/Select";
 import { useData } from "../../contexts/DataContext";
 import Modal from "../Modal";
 import ModalEvent from "../ModalEvent";
-
 import "./style.css";
 
 const PER_PAGE = 9;
@@ -14,24 +13,26 @@ const EventList = () => {
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
   const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+    (!type 
+      ? data?.events 
+      : data?.events.filter(event => !type || event.type === type)) || []
+      ).filter((_, index) => {
+      if 
+      ((currentPage - 1) * PER_PAGE <= index && 
+      PER_PAGE * currentPage > index) {
+        return true;
+      }
+      return false;
+    });
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+/* console.log(data.events) */
+  const pageNumber = Math.floor((filteredEvents?.length || 0) / { PER_PAGE }) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
+
   return (
     <>
       {error && <div>An error occured</div>}
